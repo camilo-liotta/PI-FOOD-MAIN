c5c7165afa521470ea545bd6b5c57b59f4921d14
CLiotta <camilo.liotta999@gmail.com>
2023-08-22 06:23:09 -0300

    port fix

--- api/index.js
+++ api/index.js
@@ -19,11 +19,15 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
+const {
+  PORT
+} = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log(process.version);
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
